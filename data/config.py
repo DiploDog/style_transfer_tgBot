fce734b01ef267af3d18f8f6d883b4c5af0ff032
DiploDog <mechenu@icloud.com>
2022-02-10 18:40:50 +0300

    webhooks deleted

--- data/config.py
+++ data/config.py
@@ -5,8 +5,4 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
-WEBHOOK_HOST = env.str("WEBHOOK_HOST")
-WEBHOOK_PATH = env.str("WEBHOOK_PATH")
-WEBHOOK_URL = env.str("WEBHOOK_URL")
-WEBAPP_HOST = env.str("WEBAPP_HOST")
-WEBAPP_PORT = 3001
+
